139a903cd4f61073a4bc9b484ff580479e8ab029
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-12 11:16:48 -0700

    Change search object to query api for both condition and-or doctor's name.

--- src/doctor-query.js
+++ src/doctor-query.js
@@ -1,15 +1,21 @@
-export default class SearchByCondition {
-  constructor(condition) {
+export default class DoctorSearch {
+  constructor(condition, name) {
     this.loc = `45.5204518,-122.6777959`; // Lat & Long of Epicodus
     this.key = process.env.exports.apiKey;
     this.condition = condition;
-    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${this.key}&location=${this.loc},50&sort=distance-asc&query=${this.condition}`;
+    this.doctorName = name;
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${this.key}&location=${this.loc},50&sort=distance-asc`;
+    if (this.condition) {
+      this.url = this.url + `&query=${this.condition}`;
+    }
+    if (this.doctorName) {
+      this.url = this.url + `&name=${this.doctorName}`;
+    }
   }
 
   getResults() {
     const url = this.url;
     return new Promise(function(resolve, reject) {
-      console.log("what the fuck");
       let request = new XMLHttpRequest();
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 import $ from 'jquery';
+import DoctorSearch from './doctor-query.js'
 
 
 $(document).ready(function() {
-  
+  $('form#input').submit(function(event) {
+    event.preventDefault();
+    let condition = $('#condition').val();
+    if (condition === '') {
+      condition = false;
+    }
+    let name = $('#name').val();
+    if (name === '') {
+      name = false;
+    }
+
+    const search = new DoctorSearch(condition, name);
+    console.log(search);
+  });
 });
