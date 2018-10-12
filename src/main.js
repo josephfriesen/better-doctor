ad383034b939b143d22bedcdfb40a011b8a339f9
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-12 14:26:28 -0700

    MVP. Add ability to print up to 10 results of search on page complete with name, phone, address, image, and whether they accept new patients.

--- src/main.js
+++ src/main.js
@@ -38,14 +38,31 @@ $(document).ready(function() {
           $('#num-doctors-found').text(len);
         } else if (len == 10) {
           $('#num-doctors-found').text("first 10");
-          for (let i = 0; i < len; i++) {
-            $('.doctors').append(`<div class='doctor' id='doctor-${i}'></div>`);
-            $(`#doctor-${i}`).html(`<div class='doctor-img' id='doctor-img-${i}'></div><div class='doctor-info' id='doctor-info-${i}'></div>`);
-            $(`#doctor-img-${i}`).html(`img ${i}`);
-            $(`#doctor-info-${i}`).html(`info $${i}`);
+        }
+        for (let i = 0; i < len; i++) {
+          const doctor = result.data[i];
+          const address = doctor.practices[0].visit_address;
+          const newPatients = doctor.practices[0].accepts_new_patients;
+          const phone = doctor.practices[0].phones[0].number
+            .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+          $('.doctors').append(`<div class='doctor' id='doctor-${i}'></div>`);
+          $(`#doctor-${i}`).html(`<div class='doctor-img' id='doctor-img-${i}'></div><div class='doctor-info' id='doctor-info-${i}'></div>`);
+          $(`#doctor-img-${i}`).html(`<img src="${doctor.profile.image_url}">`);
+          $(`#doctor-info-${i}`).html(`<p>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</p><ul id=${i}></ul>`);
+          if (address.street2 == undefined) {
+            $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br>
+            ${address.city}, ${address.state_long} ${address.zip}`);
+          } else {
+            $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br> ${address.street2}<br>
+            ${address.city}, ${address.state_long} ${address.zip}`);
+          }
+          $(`ul#${i}`).append(`<li><span class="list-label">Phone</span>: ${phone}</li>`);
+          if (newPatients) {
+            $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is currently accepting new patients.`);
+          } else {
+            $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is not currently accepting new patients.`);
           }
         }
-        console.log(result.data);
       }
     }, function(error) {
       $('.searching').hide();
