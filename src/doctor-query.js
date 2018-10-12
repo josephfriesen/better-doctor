d2d1d6621d54cdaa77692b027d30906a5d2d3c2b
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-12 15:20:32 -0700

    Add method to search object to query Better Doctor for complete list of specialties, then adds them to a selectable list on the page on document load.

--- src/doctor-query.js
+++ src/doctor-query.js
@@ -1,9 +1,10 @@
 export default class DoctorSearch {
-  constructor(condition, name) {
+  constructor(condition, name, specialties=false) {
     this.loc = `45.5204518,-122.6777959`; // Lat & Long of Epicodus
     this.key = process.env.exports.apiKey;
     this.condition = condition;
     this.doctorName = name;
+    this.specialties = specialties; // specialties should be false or a string of uid's separated by commas
     this.url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${this.key}&location=${this.loc},50&sort=distance-asc`;
     if (this.condition) {
       this.url = this.url + `&query=${this.condition}`;
@@ -11,10 +12,30 @@ export default class DoctorSearch {
     if (this.doctorName) {
       this.url = this.url + `&name=${this.doctorName}`;
     }
+    if (this.specialties) {
+      this.url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${this.key}&specialty_uid=${this.specialties}`;
+    }
+    this.urlSpecialties = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${this.key}`;
   }
 
   getResults() {
     const url = this.url;
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getSpecialties() {
+    const url = this.urlSpecialties;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       request.onload = function() {
--- src/main.js
+++ src/main.js
@@ -4,8 +4,55 @@ import './scss/styles.scss';
 import $ from 'jquery';
 import DoctorSearch from './doctor-query.js'
 
+// Function to take a doctor returned from the Better Doctor API and print their relevant information on the page
+const doctorDisplay = (i, result) => {
+  const doctor = result.data[i];
+  const address = doctor.practices[0].visit_address;
+  const newPatients = doctor.practices[0].accepts_new_patients;
+  const phone = doctor.practices[0].phones[0].number
+  .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+  $('.doctors').append(`<div class='doctor' id='doctor-${i}'></div>`);
+  $(`#doctor-${i}`).html(`<div class='doctor-img' id='doctor-img-${i}'></div><div class='doctor-info' id='doctor-info-${i}'></div>`);
+  $(`#doctor-img-${i}`).html(`<img src="${doctor.profile.image_url}">`);
+  $(`#doctor-info-${i}`).html(`<p>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</p><ul id=${i}></ul>`);
+  if (address.street2 == undefined) {
+    $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br>
+    ${address.city}, ${address.state_long} ${address.zip}`);
+  } else {
+    $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br> ${address.street2}<br>
+    ${address.city}, ${address.state_long} ${address.zip}`);
+  }
+  $(`ul#${i}`).append(`<li><span class="list-label">Phone</span>: ${phone}</li>`);
+  if (newPatients) {
+    $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is currently accepting new patients.`);
+  } else {
+    $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is not currently accepting new patients.`);
+  }
+}
+
 
 $(document).ready(function() {
+
+  let specialtiesList;
+  const searchSpecialties = new DoctorSearch();
+  const promise = searchSpecialties.getSpecialties();
+  promise.then(function(response) {
+    const result = JSON.parse(response);
+    specialtiesList = result.data;
+    specialtiesList.sort(function(a,b) {
+      if (a.name < b.name) {
+        return -1;
+      } else {
+        return 1
+      }
+    });
+    $('#spec-list').html('');
+    for (let i = 0; i < specialtiesList.length; i++) {
+      $('#spec-list').append(`<option value='${specialtiesList[i].uid}'>${specialtiesList[i].name}</option>`);
+    }
+    console.log(specialtiesList);
+  });
+
   $('form#input').submit(function(event) {
     event.preventDefault();
     $('.doctors').html('');
@@ -14,13 +61,9 @@ $(document).ready(function() {
     $('.no-results').hide();
     $('.error').hide();
     let condition = $('#condition').val();
-    if (condition === '') {
-      condition = false;
-    }
     let name = $('#name').val();
-    if (name === '') {
-      name = false;
-    }
+    let special = $('#spec-list').val();
+    console.log(special);
 
     const search = new DoctorSearch(condition, name);
     let result;
@@ -40,28 +83,7 @@ $(document).ready(function() {
           $('#num-doctors-found').text("first 10");
         }
         for (let i = 0; i < len; i++) {
-          const doctor = result.data[i];
-          const address = doctor.practices[0].visit_address;
-          const newPatients = doctor.practices[0].accepts_new_patients;
-          const phone = doctor.practices[0].phones[0].number
-            .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-          $('.doctors').append(`<div class='doctor' id='doctor-${i}'></div>`);
-          $(`#doctor-${i}`).html(`<div class='doctor-img' id='doctor-img-${i}'></div><div class='doctor-info' id='doctor-info-${i}'></div>`);
-          $(`#doctor-img-${i}`).html(`<img src="${doctor.profile.image_url}">`);
-          $(`#doctor-info-${i}`).html(`<p>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</p><ul id=${i}></ul>`);
-          if (address.street2 == undefined) {
-            $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br>
-            ${address.city}, ${address.state_long} ${address.zip}`);
-          } else {
-            $(`ul#${i}`).append(`<li><span class="list-label">Address</span>: ${address.street}<br> ${address.street2}<br>
-            ${address.city}, ${address.state_long} ${address.zip}`);
-          }
-          $(`ul#${i}`).append(`<li><span class="list-label">Phone</span>: ${phone}</li>`);
-          if (newPatients) {
-            $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is currently accepting new patients.`);
-          } else {
-            $(`#doctor-info-${i}`).append(`Dr. ${doctor.profile.last_name} is not currently accepting new patients.`);
-          }
+          doctorDisplay(i, result);
         }
       }
     }, function(error) {
